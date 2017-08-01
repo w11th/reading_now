cbacc30bb5b0c3eda48baf3542841b87d7965b13
Wenhui Wang <w.eleventh@gmail.com>
2017-08-01 18:26:50 +0800

    Update devise sign in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,30 @@
 module ApplicationHelper
+  APP_NAME = 'Reading Now'.freeze
+
+  def app_name
+    APP_NAME
+  end
+
+  def full_title(page_title = '')
+    if page_title.blank?
+      app_name
+    else
+      "#{page_title} | #{app_name}"
+    end
+  end
+
+  def menu_li_tag(path, text = '')
+    content_tag(:li, request.path == path ? { class: 'active' } : {}) do
+      content_tag(:a, href: path) do
+        if block_given?
+          yield
+        else
+          text
+        end
+      end
+    end
+  end
+
   def twitterlized_alert_type(type)
     case type
     when 'alert'
