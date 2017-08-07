fc2794b2887a95ecfd790413c4246ad16e33f0c5
Wenhui Wang <w.eleventh@gmail.com>
2017-08-07 12:23:42 +0800

    Bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   devise_for :users,
-             sign_out_via: [:delete, :get],
-             controllers: { sessions: 'users/sessions' }
+             sign_out_via: [:delete, :get]
 
   resources :books
 end
