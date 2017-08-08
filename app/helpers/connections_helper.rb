64f3dd0fe66c1403c7419cc7602d7f7449ffc6b2
Wenhui Wang <w.eleventh@gmail.com>
2017-08-08 18:26:06 +0800

    Add helper methods: connections_helper.rb

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -16,6 +16,53 @@ module ConnectionsHelper
     end
   end
 
-  def omniauth_data_tag
+  def provider_display_name(provider)
+    case provider
+    when 'facebook' then 'Facebook'
+    when 'github' then 'GitHub'
+    when 'douban' then '豆瓣'
+    when 'wechat' then '微信'
+    else provider
+    end
+  end
+
+  def provider_link_url(auth_data)
+    case auth_data['provider']
+    when 'facebook' then 'https://facebook.com'
+    when 'github' then 'https://github.com'
+    when 'douban' then 'https://douban.com'
+    when 'wechat' then 'https://wx.qq.com'
+    else provider
+    end
+  end
+
+  def omniauth_data_tag(action = 'sign in')
+    return unless omniauth_data
+    content_tag(:div, class: 'omniauth-data-line') do
+      content_tag(:div, class: 'checkbox') do
+        content_tag(:label) do
+          concat check_box_tag('establish_connection', '1', false)
+          concat "#{action}后绑定 #{provider_display_name(omniauth_data['provider'])} 帐号："
+          concat content_tag(:a, omniauth_data['info']&.[]('name'),
+                             href: provider_link_url(omniauth_data),
+                             target: '_blank')
+          concat image_tag(omniauth_data['info']&.[]('image'),
+                           class: 'img-circle',
+                           alt: "#{omniauth_data['provider']} avatar")
+        end
+      end
+    end
+  end
+
+  def store_omniauth_data(auth)
+    session['devise.omniauth_data'] = auth
+  end
+
+  def destroy_omniauth_data
+    session['devise.omniauth_data'] = nil
+  end
+
+  def omniauth_data
+    session['devise.omniauth_data']
   end
 end
